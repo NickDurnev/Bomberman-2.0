b56341bbc8bf4b4b0ee9a2e2f492f278ce5f55fd
NickDurnev <nikitaspec@gmail.com>
2025-01-23 00:56:37 +0200

    feat: added timer before game start

--- src/routes/game/components/RestartGameModal.tsx
+++ src/routes/game/components/RestartGameModal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import clientSocket from "@utils/socket";
 import { EndGame, GameData as Game } from "@utils/types";
 import {
@@ -18,19 +19,25 @@ type GameData = {
 const RestartGameModal = () => {
     const [gameInfo, setGameInfo] = useState<Game | null>(null);
     const [gameId, setGameId] = useState<string | null>(null);
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
     const navigate = useNavigate();
     const { setOpen } = useModal();
 
     useEffect(() => {
-        setTimeout(() => {
-            clientSocket.on("end game", onEndGame);
-            clientSocket.on("update game", handleUpdateGame);
-            clientSocket.on("launch game", handleLaunchGame);
-        }, 3000);
+        clientSocket.on("end game", onEndGame);
+        clientSocket.on("update game", handleUpdateGame);
+        clientSocket.on("launch game", handleLaunchGame);
+        clientSocket.on("start timer", () => {
+            setIsTimerRunning(true);
+        });
+
         return () => {
             clientSocket.off("end game", onEndGame);
             clientSocket.on("update game", handleUpdateGame);
             clientSocket.on("launch game", handleLaunchGame);
+            clientSocket.off("start timer", () => {
+                setIsTimerRunning(false);
+            });
         };
     }, []);
 
@@ -63,6 +70,10 @@ const RestartGameModal = () => {
         navigate("/");
     };
 
+    const startTimer = () => {
+        clientSocket.emit("start timer", gameId);
+    };
+
     const players = gameInfo ? Object.values(gameInfo.players) : [];
     const canStartGame = players.length > 1;
 
@@ -78,21 +89,47 @@ const RestartGameModal = () => {
                                 size={"small"}
                             />
                         )}
-                        <div className="mt-6 flex flex-col justify-center items-center mx-auto gap-y-8">
-                            <Button
-                                text="Restart Game"
-                                animated
-                                animatedIcon={"💣"}
-                                onClick={startGameAction}
-                                // disabled={!canStartGame}
-                            />
-                            <Button
-                                text="Main Menu"
-                                animated
-                                animatedIcon={"🚪"}
-                                onClick={leaveGameAction}
-                            />
-                        </div>
+                        {isTimerRunning ? (
+                            <div className="flex flex-col gap-y-8">
+                                <h3 className="text-3xl font-bold tracking-wider text-center motion-preset-expand motion-loop-once">
+                                    Game will start in
+                                </h3>
+                                <div className="flex justify-center">
+                                    <CountdownCircleTimer
+                                        isPlaying
+                                        duration={3}
+                                        colors="#8953AD"
+                                        strokeWidth={10}
+                                        onComplete={() => {
+                                            setIsTimerRunning(false);
+                                            startGameAction();
+                                        }}
+                                    >
+                                        {({ remainingTime }) => (
+                                            <div className="text-4xl font-bold">
+                                                {remainingTime}
+                                            </div>
+                                        )}
+                                    </CountdownCircleTimer>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="mt-6 flex flex-col justify-center items-center mx-auto gap-y-8">
+                                <Button
+                                    text="Restart Game"
+                                    animated
+                                    animatedIcon={"💣"}
+                                    onClick={startTimer}
+                                    // disabled={!canStartGame}
+                                />
+                                <Button
+                                    text="Main Menu"
+                                    animated
+                                    animatedIcon={"🚪"}
+                                    onClick={leaveGameAction}
+                                />
+                            </div>
+                        )}
                     </ModalContent>
                 </ModalBody>
             </Modal>
--- src/routes/pending/Pending.tsx
+++ src/routes/pending/Pending.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import clientSocket from "@utils/socket";
 import { GameData as Game } from "@utils/types";
 import { Button, PlayersSlots, UserBar } from "@components/index";
@@ -12,16 +13,23 @@ const PendingGame = () => {
     const { gameId } = useParams();
     const navigate = useNavigate();
     const [gameInfo, setGameInfo] = useState<Game | null>(null);
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
 
     useEffect(() => {
         clientSocket.on("update game", handleUpdateGame);
         clientSocket.on("launch game", handleLaunchGame);
+        clientSocket.on("start timer", () => {
+            setIsTimerRunning(true);
+        });
 
         clientSocket.emit("enter pending game", gameId);
 
         return () => {
             clientSocket.off("update game", handleUpdateGame);
             clientSocket.off("launch game", handleLaunchGame);
+            clientSocket.off("start timer", () => {
+                setIsTimerRunning(false);
+            });
         };
     }, [gameId]);
 
@@ -39,6 +47,10 @@ const PendingGame = () => {
         navigate("/");
     };
 
+    const startTimer = () => {
+        clientSocket.emit("start timer", gameId);
+    };
+
     const startGameAction = () => {
         clientSocket.emit("start game", gameId);
         handleLaunchGame(); // Trigger the game launch
@@ -46,6 +58,14 @@ const PendingGame = () => {
 
     const players = gameInfo ? Object.values(gameInfo.players) : [];
     const canStartGame = players.length > 1;
+
+    // const renderTime = ({ remainingTime }: { remainingTime: number }) => {
+    //     // if (remainingTime === 0) {
+    //     //     return <div className="timer">Too lale...</div>;
+    //     // }
+
+    //     return <div className="text-4xl font-bold">{remainingTime}</div>;
+    // };
 
     return (
         <>
@@ -56,21 +76,47 @@ const PendingGame = () => {
                         {gameInfo?.name ?? "Game"}
                     </h1>
                 </div>
-                <div className="mt-6 flex flex-col justify-center items-center mx-auto gap-y-8">
-                    <Button
-                        text="Start Game"
-                        animated
-                        animatedIcon={"💣"}
-                        onClick={startGameAction}
-                        // disabled={!canStartGame}
-                    />
-                    <Button
-                        text="Leave Game"
-                        animated
-                        animatedIcon={"🚪"}
-                        onClick={leaveGameAction}
-                    />
-                </div>
+                {isTimerRunning ? (
+                    <div className="flex flex-col gap-y-8">
+                        <h3 className="text-3xl font-bold tracking-wider text-center motion-preset-expand motion-loop-once">
+                            Game will start in
+                        </h3>
+                        <div className="flex justify-center">
+                            <CountdownCircleTimer
+                                isPlaying
+                                duration={3}
+                                colors="#8953AD"
+                                strokeWidth={10}
+                                onComplete={() => {
+                                    setIsTimerRunning(false);
+                                    startGameAction();
+                                }}
+                            >
+                                {({ remainingTime }) => (
+                                    <div className="text-4xl font-bold">
+                                        {remainingTime}
+                                    </div>
+                                )}
+                            </CountdownCircleTimer>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="mt-6 flex flex-col justify-center items-center mx-auto gap-y-8">
+                        <Button
+                            text="Start Game"
+                            animated
+                            animatedIcon={"💣"}
+                            onClick={startTimer}
+                            // disabled={!canStartGame}
+                        />
+                        <Button
+                            text="Leave Game"
+                            animated
+                            animatedIcon={"🚪"}
+                            onClick={leaveGameAction}
+                        />
+                    </div>
+                )}
                 {gameInfo && (
                     <PlayersSlots
                         max_players={gameInfo.max_players}
